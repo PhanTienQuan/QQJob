986a6f4586dd944807c2b095ceaf1e8e325b4c67
PhanTienQuan <phantienquan1509@gmail.com>
2024-12-05 22:32:01 +0700

    adding job list and hob post detail UI

---
+++ QQJob/Controllers/JobsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QQJob.Controllers
+{
+    public class JobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
