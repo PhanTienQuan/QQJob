a897b87c5470b2ebfa993e22e9d18d7e69e565b4
Quyền Phạm <phamquocquyen0202@gmail.com>
2024-12-22 15:43:39 +0700

    candidate

---
+++ QQJob/Controllers/CandidateController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QQJob.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult AppliedJob()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+        public IActionResult Meeting()
+        {
+            return View();
+        }
+        public IActionResult Follow()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        public IActionResult DeleteProfile()
+        {
+            return View();
+        }
+    }
+}
