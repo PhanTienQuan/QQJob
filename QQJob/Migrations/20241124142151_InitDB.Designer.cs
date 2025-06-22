5db4a5f9bd9f0b75a92e0b2f1ef29fcac4301f96
Quyền Phạm <phamquocquyen0202@gmail.com>
2025-06-22 21:10:39 +0700

    Revert "merge"
    This reverts commit d072181c0897e8f66bfa6129834d1a7cde9a8db6.

--- QQJob/Migrations/20241124142151_InitDB.Designer.cs
+++ QQJob/Migrations/20241124142151_InitDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QQJob.Data;
 
@@ -11,9 +12,11 @@ using QQJob.Data;
 namespace QQJob.Migrations
 {
     [DbContext(typeof(QQJobContext))]
-    partial class QQJobContextModelSnapshot : ModelSnapshot
+    [Migration("20241124142151_InitDB")]
+    partial class InitDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- QQJob/Migrations/20241205171407_V1.Designer.cs
+++ QQJob/Migrations/20241205171407_V1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QQJob.Data;
 
@@ -11,9 +12,11 @@ using QQJob.Data;
 namespace QQJob.Migrations
 {
     [DbContext(typeof(QQJobContext))]
-    partial class QQJobContextModelSnapshot : ModelSnapshot
+    [Migration("20241205171407_V1")]
+    partial class V1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -264,6 +267,8 @@ namespace QQJob.Migrations
                         .IsUnique()
                         .HasDatabaseName("UserNameIndex")
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
+
+                    b.HasIndex("UserName");
 
                     b.ToTable("AspNetUsers", (string)null);
                 });
